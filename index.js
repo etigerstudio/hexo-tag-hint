557a552915122afce75f5c7b5b19267df17c3945
etigerstudio <etigerstudio@gmail.com>
2020-12-06 15:27:16 +0800

    impl: multi-line hint text support.
    refactor: adopt some ES6 syntax.

--- index.js
+++ index.js
@@ -3,54 +3,35 @@
 /**
  * hexo-tag-hint
  * https://github.com/etigerstudio/hexo-tag-hint
- * Copyright (c) 2017-2018, E-Tiger Studio
- * Licensed under the GNU AGPLv3 license.
+ * Copyright (c) 2017-2020, E-Tiger Studio
+ * Licensed under the MIT license.
  *
  * Syntax:
- *  {% hint 'body_text' 'hint_text' %}
+ *   Single-line: {% hint 'body_text' 'hint_text' %}
+ *   Multi-line: {% hint 'body_text' 'hint_text_1st_line' 'hint_text_2nd_line' ... %}
  */
 
-// var counter = 0;
-// var fs = require('hexo-fs'),
-//     util = require('hexo-util'),
-//     path = require('path'),
-//     assetDir = 'assets',
-//     cssDir = 'css',
-//     fileName = 'hint.min.css',
-//     srcPath = path.join(cssDir, fileName);
-//
-// var tagHintConfig = hexo.config.tag_hint;
-// var filePath;
-// if (tagHintConfig && tagHintConfig.use_cdn && tagHintConfig.cdn_base_url) {
-//     filePath = path.join(tagHintConfig.cdn_base_url, srcPath);
-// } else {
-//     filePath = path.join(assetDir, srcPath);
-//     var physicalPath = path.join(__dirname, srcPath);
-//     hexo.extend.generator.register(fileName, function() {
-//         return {
-//             path: filePath,
-//             data: function data() {
-//                 return fs.createReadStream(physicalPath);
-//             }
-//         };
-//     });
-// }
-
-var util = require('hexo-util');
+const util = require('hexo-util');
+const version = '0.2.1';
+const styleClass = 'hint--info hint--rounded hint--top';
+const newLineDelimiter = '\u000A';
 
 hexo.extend.filter.register('after_post_render', function(data) {
     data.content =
-        util.htmlTag('link', {rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/npm/hexo-tag-hint@0.2.1/css/hint.min.css'}) +
+        util.htmlTag(
+            'link',
+            {
+                rel: 'stylesheet',
+                type: 'text/css',
+                href: `https://cdn.jsdelivr.net/npm/hexo-tag-hint@${version}/dist/hexo-tag-hint.min.css`
+            }) +
         data.content;
-    // var root = hexo.config.root ? hexo.config.root : '/';
-    // data.content = '<link rel="stylesheet" href="' + path.join('/', filePath) + '"></link>' + data.content;
     return data;
 });
 
-// {% hint 'body_text' 'hint_text' %}
+// {% hint 'body_text' 'hint_text_1st_line' 'hint_text_2nd_line' ... %}
 hexo.extend.tag.register('hint', function(args) {
-    var raw = '<span style="border-bottom: 1px dashed #389fff;">' +
-    '<span class="hint--top hint--rounded hint--info" aria-label="' +
-    args[1] + '" ontouchstart>' + args[0] + '</span></span>'
-    return raw;
+    let body = args[0];
+    let hint = args.slice(1).join(newLineDelimiter);
+    return `<span class="${styleClass}" data-hint="${hint}" ontouchstart>${body}</span>`;
 });
